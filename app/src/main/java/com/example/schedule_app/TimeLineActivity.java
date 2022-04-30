c8714ffa4df5acfbaa33df905cdfca0fa80018be
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-04-30 14:02:01 +0200

    Use Executors

--- app/src/main/java/com/example/schedule_app/TimeLineActivity.java
+++ app/src/main/java/com/example/schedule_app/TimeLineActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.example.schedule_app.adapter.TimeLineAdapter;
 
 import java.util.Objects;
+import java.util.concurrent.Executors;
 
 public class TimeLineActivity extends AppCompatActivity {
     Data data;
@@ -18,6 +21,7 @@ public class TimeLineActivity extends AppCompatActivity {
     Button toSchedule, toSettings;
     Background background;
     Date deviceDate;
+    LinearLayout navBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +35,32 @@ public class TimeLineActivity extends AppCompatActivity {
         toSettings = findViewById(R.id.toSettings);
         timeLineView = findViewById(R.id.TimeLineListView);
         background = new Background(getApplicationContext(), this);
+        navBar = findViewById(R.id.TimeLineNavBar);
+
 
         if(data.getLastStoredDate() != null && !data.getLastStoredDate().equals(deviceDate.getTodayDate()) && data.getStoredSchedule() !=null){
-            data.storeScheduleObjects(WebScraper.scrape(data.getScheduleURL()));
-            data.putLastStoredDate(deviceDate.getTodayDate());
-        }
+            Executors.newSingleThreadExecutor().execute(() ->{
+                data.removeStoredSchedule();
+                data.storeScheduleObjects(WebScraper.scrape(data.getScheduleURL()));
 
-        timeLineAdapter = new TimeLineAdapter(this, data.getStoredSchedule());
-        timeLineView.setAdapter(timeLineAdapter);
+                runOnUiThread(()->{
+                    data.putLastStoredDate(deviceDate.getTodayDate());
+                    setUpAdapter();
+                });
+            });
+        }else{
+            setUpAdapter();
+        }
 
         toSchedule.setOnClickListener(View -> startActivity(new Intent(getApplicationContext(), MainActivity.class).putExtra("Navigate", true)));
         toSettings.setOnClickListener(View -> startActivity(new Intent(getApplicationContext(), SettingsActivity.class)));
 
     }
 
+    public void setUpAdapter(){
+        timeLineAdapter = new TimeLineAdapter(this, data.getStoredSchedule());
+        timeLineView.setAdapter(timeLineAdapter);
+    }
 
     @Override
     protected void onDestroy() {
@@ -56,6 +72,6 @@ public class TimeLineActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         data.onOpenLayout(this.getClass().getSimpleName());
-        timeLineAdapter.getTranslator().closeTranslationModel();
+        //timeLineAdapter.getTranslator().closeTranslationModel();
     }
 }
